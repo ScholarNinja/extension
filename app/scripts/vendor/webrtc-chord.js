27b482ca1cb3ea39ae64348e2c585849deec8611
Jure Triglav <juretriglav@gmail.com>
2014-07-16 13:36:10 +0200

    Update webrtc-chord to newest master.

--- app/scripts/vendor/webrtc-chord.js
+++ app/scripts/vendor/webrtc-chord.js
@@ -1,7 +1,7 @@
 !function(e){if("object"==typeof exports&&"undefined"!=typeof module)module.exports=e();else if("function"==typeof define&&define.amd)define([],e);else{var f;"undefined"!=typeof window?f=window:"undefined"!=typeof global?f=global:"undefined"!=typeof self&&(f=self),f.Chord=e()}}(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);throw new Error("Cannot find module '"+o+"'")}var f=n[o]={exports:{}};t[o][0].call(f.exports,function(e){var n=t[o][1][e];return s(n?n:e)},f,f.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(_dereq_,module,exports){
 (function (global){
 (function() {
-  var lodash = (typeof window !== "undefined" ? window._ : typeof global !== "undefined" ? global._ : null);
+  var _ = (typeof window !== "undefined" ? window._ : typeof global !== "undefined" ? global._ : null);
   var Utils = _dereq_('./Utils');
 
   var CheckPredecessorTask = function(references) {
@@ -2264,6 +2264,14 @@
       onPeerSetup(id);
     });
 
+    this._peer.on('disconnected', function() {
+      if (!self._peer.destroyed) {
+        Utils.debug("Reconnect to the server.");
+
+        self._peer.reconnect();
+      }
+    });
+
     this._peer.on('error', function(error) {
       Utils.debug("Peer error:", error);
 
@@ -2294,9 +2302,7 @@
         throw new Error("Invalid state.");
       }
 
-      var conn = this._peer.connect(peerId, {
-        serialization: 'json'
-      });
+      var conn = this._peer.connect(peerId);
       if (!conn) {
         var error = new Error("Failed to open connection to " + peerId + ".");
         this._callbacks.onConnectionOpened(peerId, null, error);
